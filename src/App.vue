bbb47fea8babb091fc50246b026120436c9b7f57
edpyt <edrsa1321s@gmail.com>
2025-03-27 09:00:31 +0500

    feat: tauri app with tailwind

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from "vue";
+// TODO:
+// import { invoke } from "@tauri-apps/api/core";
+
+enum CalculatorMathematicalOperations {
+  Plus = "+",
+  Minus = "-",
+  Division = "/",
+  Multiplication = "*",
+}
+
+const currentMathematicalOperation = ref();
+
+// TODO: calculate mathematical operations in Rust
+// :^)
+// async function greet() {
+//   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+//   greetMsg.value = await invoke("greet", { name: name.value });
+// }
+</script>
+
+<template>
+  <main class="container">
+    <div>
+      <input type="text" />
+
+      <div v-for="idx in 3" :key="idx">
+        <button v-for="idxx in 3" :key="idxx">{{ idxx }}</button>
+      </div>
+
+      <button v-for="mathOperation in CalculatorMathematicalOperations">
+        {{ mathOperation }}
+      </button>
+    </div>
+    selected operation: {{ currentMathematicalOperation }}
+  </main>
+</template>
